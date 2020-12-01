da37a0d8775ed2187b855f2b8f4504adb5079182
rustam <tabarzin@gmail.com>
2020-12-01 23:18:46 +0300

    Added fonts

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// JS
+//import './js/'
+//require('./main.scss');
+// SCSS
+
+import './assets/scss/main.scss';
+//import './components/colors_type/colors-type.scss';
+
+
+
+
+function requireAll(requireContext) {
+  return requireContext.keys().map(requireContext);
+}
+
+requireAll(require.context('./pages', true, /\.(scss|js)$/));
+//requireAll(require.context('./assets/favicons/', true, /\.js$/));
